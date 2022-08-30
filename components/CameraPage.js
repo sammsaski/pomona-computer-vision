c8d1900476b917570e1795e79b8d05518b397359
sammsaski <samuel.sasaki@gmail.com>
2022-08-29 22:34:24 -0700

    Building frontend

---
+++ components/CameraPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions, Image } from 'react-native';
+import { Camera } from 'expo-camera';
+import { manipulateAsync, FlipType, SaveFormat } from 'expo-image-manipulator';
+
+// import * as tf from '@tensorflow/tfjs';
+
+
